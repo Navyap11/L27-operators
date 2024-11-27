392dc809580d7073d5abe67ac6c080bac3f19b70
navyap11 <ashwin.p.parmar@gmail.com>
2024-11-27 16:03:47 +0100

    h

---
+++ Grades.py
@@ -0,0 +1,12 @@
+print("Enetr Marks Obtained in 4 subjects: ")
+maths= int(input("maths: "))
+english= int(input("english: "))
+science= int(input("science: "))
+hindi= int(input("hindi: " ))
+
+sum= maths+english+science+hindi 
+print("sum of maths.english,science and hindi")
+perc= (sum/400)*100
+
+print(end= "Pencentage Mark = ")
+print(perc)
